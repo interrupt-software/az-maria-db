24e88baff6de53441cb0d457d23ed90e2e5aba26
gcastill0 <gilberto@hashicorp.com>
2021-03-30 10:47:37 -0400

    Initial sample

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
+
+resource "azurerm_mariadb_server" "mariadb" {
+  name                = "${var.prefix}-mariadb"
+  resource_group_name = "${var.prefix}-rg"
+  location            = var.location
+
+  administrator_login          = var.db_admin
+  administrator_login_password = var.db_password
+
+  sku_name   = "B_Gen5_2"
+  storage_mb = 5120
+  version    = "10.2"
+
+  auto_grow_enabled             = true
+  backup_retention_days         = 7
+  geo_redundant_backup_enabled  = false
+  public_network_access_enabled = true
+  ssl_enforcement_enabled       = true
+}
+
+resource "azurerm_mariadb_database" "sample" {
+  name                = var.db_name
+  resource_group_name = "${var.prefix}-rg"
+  server_name         = "${var.prefix}-mariadb"
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "prefix" {}
+variable "location" {}
+variable "db_name" {}
+variable "db_admin" {}
+variable "db_password" {}
