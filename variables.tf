bf8fb451a033822014d3324486182b022edc6482
gcastill0 <gilberto@hashicorp.com>
2021-03-30 11:02:00 -0400

    made change, now

--- variables.tf
+++ variables.tf
@@ -1,5 +1,11 @@
-variable "prefix" {}
-variable "location" {}
-variable "db_name" {}
+variable "prefix" {
+  default = "interrupt"
+}
+variable "location" {
+  default = "Canada East"
+}
+variable "db_name" {
+  default = "demo"
+}
 variable "db_admin" {}
 variable "db_password" {}
